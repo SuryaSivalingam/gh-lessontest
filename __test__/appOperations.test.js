3594c2485b34770d35674f3c61877fb1733b2923
surya <suryas1697@gmail.com>
2025-11-29 13:36:38 +0530

    fixed errors in js file

--- __test__/appOperations.test.js
+++ __test__/appOperations.test.js
@@ -1,5 +1,9 @@
 const appOperations = require('../src/appOperations');
 
 test("multiplication of two numbers", ()=>{
-    expect(appOperations(12,0).toBe(0));
+    expect(appOperations.multiply(12,0).toBe(0));
+});
+
+test("addition of two numbers", ()=>{
+    expect(appOperations.multiply(12,27).toBe(39));
 });
