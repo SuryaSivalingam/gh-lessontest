8b6417296b979755b2772060509af81318d2a233
surya <suryas1697@gmail.com>
2025-11-29 13:42:49 +0530

    fixed the errors in add and multiply function

--- __test__/appOperations.test.js
+++ __test__/appOperations.test.js
@@ -1,9 +1,9 @@
 const appOperations = require('../src/appOperations');
 
 test("multiplication of two numbers", ()=>{
-    expect(appOperations.multiply(12,0).toBe(0));
+    expect(appOperations.multiply(12,0)).toBe(0);
 });
 
 test("addition of two numbers", ()=>{
-    expect(appOperations.add(12,27).toBe(39));
+    expect(appOperations.add(12,27)).toBe(39);
 });
